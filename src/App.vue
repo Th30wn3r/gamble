d4190424ae1a442f7bbe265a2aa31c06de551073
Th30wn3r <87546479+Th30wn3r@users.noreply.github.com>
2022-02-17 20:12:22 -0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <vault-header />
+
+    <router-view />
+  </main>
+</template>
+
+<script>
+import VaultHeader from "@/components/VaultHeader";
+
+export default {
+  name: "App",
+  components: { VaultHeader }
+};
+</script>
